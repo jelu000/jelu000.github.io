88d82cf4b437c576f2c71f3e9304ec24cda249aa
Jens Lundeqvist <jens.lundeqvist@storsjogymnasiet.se>
2023-05-16 22:01:44 +0200

    klar

--- jokeswebb/joke.js
+++ jokeswebb/joke.js
@@ -1,9 +1,15 @@
 //https://foolishdeveloper.com/random-joke-generator-using-javascript-api/
+//https://v2.jokeapi.dev/#url-parameters
 
 const jokebutt = document.getElementById("jokebutton");
+
 jokebutt.addEventListener("click", getJoke)
 
+const p_joke = document.getElementById("pjoke")
+
 function getJoke(e) {
+
+    
 
     fetch(
         `https://v2.jokeapi.dev/joke/Any?blacklistFlags=nsfw,religious,political,racist,sexist,explicit&type=single`,
@@ -17,14 +23,13 @@ function getJoke(e) {
         .then(res => res.json())
         .then(response => {
             
-            console.log(`${response.joke}`)
-            
-            //psvar.innerHTML= prsnr  + " " + getGender(prsnr);
+            //console.log(`${response.joke}`)
+            let joke = response.joke
+            pjoke.innerHTML = joke;
 
             
         })
         .catch(error => console.log(error));
-
 }
 
 
