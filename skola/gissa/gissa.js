fee8757c16fe634686379216fc0b734293838512
Jens <jelu000@gmail.com>
2023-08-15 23:35:55 +0200

    fixade alla länkar

--- skola/gissa/gissa.js
+++ skola/gissa/gissa.js
@@ -11,6 +11,41 @@ function gissaTal(){
     let vinn = false;
     let p_svar = document.getElementById("p_svar");
 
+
+    if (antalgissningar < 3){
+
+        if (slumptal === gissa_int){
+            console.log("Rätt svar!");
+            vinn = true;
+            p_svar.innerHTML = "YOU ARE A WINNER!";
+            antalgissningar = 3;
+        }
+        else {
+            console.log("Fel svar!");
+
+            if (antalgissningar === 2)
+                p_svar.innerHTML= "Looser!"
+            else
+                p_svar.innerHTML=`Fel svar! ${textfelt_gissa}`
+
+            document.getElementById("gissa").value="";
+            document.getElementById("gissa").focus();
+
+        }
+        antalgissningar++;
+
+    }
+    else{
+         p_svar.innerHTML=`Looser!`
+    }
+
     console.log("textfelt=" + textfelt_gissa)
 
+}
+
+
+function myRelode(){
+
+    document.getElementById("gissa").value="";
+    location.reload();
 }
