05f0425be5d51ceef1affc4671f1e7393e44d818
Jens Lundeqvist <jens.lundeqvist@storsjogymnasiet.se>
2023-09-14 21:30:16 +0200

    måste fixa stilmallen så inte allt hoppar

--- lotta_personer/lotta_person.js
+++ lotta_personer/lotta_person.js
@@ -1,14 +1,42 @@
+//skapar array för namn
 let array_namn = [];
 
-
-//Hämtar knapp
+//Hämtar elemment
 const addknapp = document.getElementById("add_butt");
 const namntext = document.getElementById("tf_namn");
+const namnlistan = document.getElementById("namn_lista");
+const p_svar = document.getElementById("p_svar");
+const slumpa_butt = document.getElementById("slump_butt");
 
-addknapp.addEventListener("click", addPerson)
+addknapp.addEventListener("click", addPerson);
+slumpa_butt.addEventListener("click", slumpaNamn);
 
 function addPerson(){
+     
+    //Lägger till namn till arrayen
+    array_namn.push(namntext.value);
 
-    alert(`namn= ${namntext.value}`)
+    //Tömmer list elementet på namn
+    namnlistan.innerHTML = "";
+    //Skriver ut arrayen med namn
+    for (let i in array_namn) {
+        console.log(array_namn[i]);
+        namnlistan.innerHTML += `<li>${array_namn[i]}</li>`
+    }
+    //Tömmer textfältet för namn
+    namntext.value = "";
+}
 
+function slumpaNamn(){
+    let array_langd = array_namn.length;
+    const slumptal = Math.floor(Math.random() * array_langd);
+
+    p_svar.innerHTML = `VALT NAMN BLEV: ${array_namn[slumptal]}`
+}
+
+function rensaSidan(){
+    array_namn = [];
+    namntext.value = "";
+    namnlistan.innerHTML = "";
+    p_svar.innerHTML = "";
 }
