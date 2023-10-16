96fd959b9541bf6c9163bed8cc30b1e63d03bed1
Jens Lundeqvist <jens.lundeqvist@storsjogymnasiet.se>
2023-10-16 14:58:32 +0200

    Nu startar glosförhöret iglos programmet

--- glosorwebb/glosor.js
+++ glosorwebb/glosor.js
@@ -7,20 +7,28 @@ class Glosa{
     }
 }
 
+let test_time=false;
 let gloslista = [];
+const add_glosor_div = document.getElementById("add_glosor");
 const sweglosa = document.getElementById("svensk_glosa");
 const englosa = document.getElementById("engelsk_glosa");
 const addbutt = document.getElementById("add_button");
 addbutt.addEventListener("click", addGlosa);
 const show_glosor = document.getElementById("show_glosor");
+let input_glos_svar = document.getElementById("glos_svar");
+let button_starta_glostest = document.getElementById("start_glostest");
+button_starta_glostest.addEventListener("click", startaGlosTest);
 
 function addGlosa(){
 
     console.log(`swe=${sweglosa.value}, eng=${englosa.value} `);
-
     let t_glosa = new Glosa(sweglosa.value, englosa.value);
     gloslista.push(t_glosa);
 
+    sweglosa.value = "";
+    englosa.value = "";
+
+    sweglosa.focus();
     printGlosor();
 
 }
@@ -37,3 +45,17 @@ function printGlosor(){
     show_glosor.innerHTML = html_string;
 
 }
+
+function startaGlosTest(){
+
+    add_glosor_div.innerHTML = ""
+
+}
+
+input_glos_svar.addEventListener("keydown", (event) => {
+    if (event.key === "Enter") {
+        // Enter key was pressed
+        // Add your code to handle the Enter key press here
+        console.log("Enter key pressed");
+    }
+});
