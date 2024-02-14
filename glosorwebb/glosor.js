e01546d7e3db58260b3c148cb4b3b6c1c633c01f
jelu000 <jelu000@gmail.com>
2024-02-14 17:22:36 +0100

    fixade poäng, om text felt ät tomt och allaknappar

--- glosorwebb/glosor.js
+++ glosorwebb/glosor.js
@@ -23,8 +23,6 @@ const show_glosor = document.getElementById("show_glosor");
 let button_starta_glostest = document.getElementById("start_glostest");
 button_starta_glostest.addEventListener("click", startaGlosTest);
 
-//let id_print_glosa_div = document.getElementById("id_print_glosa_div")
-
 let glos_test_div = document.getElementById("glos_test_div");
 glos_test_div.style.display = "none";
 let input_glos_svar = document.getElementById("glos_svar");
@@ -35,35 +33,30 @@ let p_ratta_svar = document.getElementById("p_ratta_svar");
 let p_slut = document.getElementById("p_slut");
 
 
-/**getDataFromLocalStorage() Hämtar data localstorage */ 
-async function getDataFromLocalStorage(){
+/**getDataFromLocalStorage() Hämtar data localstorage */
+async function getDataFromLocalStorage() {
     try {
-          
-        gloslista = await JSON.parse(localStorage.getItem(localstorage_namn) );
+
+        gloslista = await JSON.parse(localStorage.getItem(localstorage_namn));
 
         //Om billistan  är tom Null från localStorage
-        if (gloslista == null){
+        if (gloslista == null) {
             gloslista = []
             show_glosor.innerHTML = "";
         }
-        else{
+        else {
             printGlosor();
         }
-        
-        
     }
-    catch (e){
-        
+    catch (e) {
+
         console.log(`Fel: ${e}`)
     }
 }
 
 getDataFromLocalStorage();
 
-
-
 //const gloslistatest = [new Glosa("katt", "cat"), new Glosa("hund", "dog"), new Glosa("bil", "car")];
-
 //console.log(gloslistatest)
 
 /*------------------------------------------------------------------
@@ -71,19 +64,26 @@ Dessa är för att lägga till glosor
 ---------------------------------------------------------------------*/
 function addGlosa() {
 
-    console.log(`swe=${sweglosa.value}, eng=${englosa.value} `);
-    let t_glosa = new Glosa(sweglosa.value, englosa.value);
-    gloslista.push(t_glosa);
-    
-        
-    localStorage.setItem(localstorage_namn , JSON.stringify(gloslista));
+    if (sweglosa.value === "" || englosa.value === "") {
+        alert("Fyll i Glosa! ")
 
-    sweglosa.value = "";
-    englosa.value = "";
+    }
+    else {
+        console.log(`swe=${sweglosa.value}, eng=${englosa.value} `);
+        let a_swglosa = sweglosa.value;
+        let a_englosa = englosa.value;
+        let t_glosa = new Glosa(a_swglosa.toLowerCase(), a_englosa.toLowerCase());
+        gloslista.push(t_glosa);
 
-    sweglosa.focus();
-    printGlosor();
 
+        localStorage.setItem(localstorage_namn, JSON.stringify(gloslista));
+
+        sweglosa.value = "";
+        englosa.value = "";
+
+        sweglosa.focus();
+        printGlosor();
+    }
 }
 
 function printGlosor() {
@@ -96,6 +96,15 @@ function printGlosor() {
     });
 
     show_glosor.innerHTML = html_string;
+}
+
+function delGlosorLocalStorage(){
+    console.log("Del click")
+    tom_array = [];
+
+    localStorage.setItem(localstorage_namn , JSON.stringify(tom_array));
+    gloslista = tom_array;
+    printGlosor()
 
 }
 
@@ -109,15 +118,9 @@ function startaGlosTest() {
     //Tömmer allt i add_glosor div
     add_glosor_div.innerHTML = ""
 
-    //skriver ut första glosan
-    //gloslista.forEach(element => {
-    //printGlosaForhor(element.swe, element.eng)
-
+    
     if (gloslista.length > 0)
         visaGlosa()
-
-
-    //});
 
 
     glos_test_div.style.display = "block"
@@ -136,36 +139,22 @@ function visaGlosa() {
 
 }
 
-function printGlosaForhor(t_sweglosa, t_engglosa) {
-
-    let html_str = `<b id='p_id_sweglosa'>${t_sweglosa}</b><b>:</b><input id='glos_svar' type='text' width='10' maxlength='50'></input>`
-    id_print_glosa_div.innerHTML = html_str
-
-    const t_input_svaret = document.getElementById("glos_svar")
-
-
-    console.log(`Glosa=${t_sweglosa}, EngGlosa=${t_engglosa} och user_svaret=${t_input_svaret.value} `)
-
-}
-
 function rattaGlosa() {
 
 
     try {
-        user_svar = input_glos_svar.value;
+        user_svar = input_glos_svar.value.toLowerCase();
         //console.log(`index: ${int_glosnr} usersvat= ${user_svar} och rätt svar= ${gloslistatest[int_glosnr].eng}`)
 
 
-
-        //if (int_glosnr < gloslistatest.length) {
-
         if (user_svar === gloslista[int_glosnr].eng) {
             //alert(`Rätt! Svaret är ${gloslistatest[int_glosnr].eng} `);
-            p_ratta_svar.innerHTML = `Rätt!  ${gloslista[int_glosnr].swe}  = ${gloslista[int_glosnr].eng} `;
             int_antalratt++;
+            p_ratta_svar.innerHTML = `Rätt!  ${gloslista[int_glosnr].swe}  = ${gloslista[int_glosnr].eng} Du har ${int_antalratt}/${gloslista.length}`;
+            
         }
         else {
-            p_ratta_svar.innerHTML = `Fel! ${user_svar}! rätt svar ${gloslista[int_glosnr].swe} = ${gloslista[int_glosnr].eng} `
+            p_ratta_svar.innerHTML = `Fel! ${user_svar}! rätt svar ${gloslista[int_glosnr].swe} = ${gloslista[int_glosnr].eng} Du har ${int_antalratt}/${gloslista.length} `
         }
 
         int_glosnr++
@@ -174,9 +163,9 @@ function rattaGlosa() {
             visaGlosa()
         else
             p_slut.innerHTML = `Glosförhöret är slut, du fick ${int_antalratt} rätt av ${gloslista.length} `
-        
+
         input_glos_svar.focus();
-        
+
 
     }
     catch (err) {
@@ -184,6 +173,8 @@ function rattaGlosa() {
     }
 
 }
+
+
 
 // Enter key was pressed
 // Add your code to handle the Enter key press here
